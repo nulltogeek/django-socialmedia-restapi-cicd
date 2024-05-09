8e242f4a18aba774267cb555b4a0ccf42cca4108
nulltogeek <alireza.moghise.dev@gmail.com>
2024-05-09 20:58:30 +0330

    feat:config JWT & add JWT urls and authentication url

--- socialmedia/api/urls.py
+++ socialmedia/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("users/", include(("socialmedia.users.urls", "users"))),
+    path("auth/", include(("socialmedia.authentication.urls", "auth"))),
 ]
