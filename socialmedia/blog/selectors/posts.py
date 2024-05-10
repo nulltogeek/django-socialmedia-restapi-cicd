8ad14db6b4bfdd247614b533dd075858e4a77562
nulltogeek <alireza.moghise.dev@gmail.com>
2024-05-10 12:52:25 +0330

    Implement post detail and post list views
    This commit introduces the post_detail and post_list views in the social media blog API. These views handle retrieving detailed information about a specific post and listing posts based on various filters, respectively.
    
    - Added the post_detail view, which retrieves a post with the specified slug while considering the subscriptions of the current user. If 'self_include' is set to True, the current user's posts are included in the result.
    - Implemented the post_list view, which lists posts while applying optional filters. The view filters posts based on the subscriptions of the current user. If 'self_include' is set to True, the current user's posts are included in the result.
    - Utilized the PostFilter class for filtering posts in the post_list view.
    
    These views enhance the functionality of the social media blog API by allowing users to view detailed information about posts and filter posts based on their subscriptions.

---
+++ socialmedia/blog/selectors/posts.py
@@ -0,0 +1,35 @@
+from django.db.models import QuerySet
+from socialmedia.blog.models import Post, Subscription
+from socialmedia.users.models import BaseUser
+from socialmedia.blog.filters import PostFilter
+
+
+def get_subscribers(*, user: BaseUser) -> QuerySet[Subscription]:
+    return Subscription.objects.filter(subscriber=user)
+
+
+def post_detail(
+    *, slug: str, user: BaseUser, self_include: bool = True
+) -> QuerySet[Post]:
+    # IDs of users that the current user is subscribed to
+    subscriptions = list(
+        Subscription.objects.filter(subscriber=user).values_list("target", flat=True)
+    )
+    if self_include:
+        subscriptions.append(user.id)
+    return Post.objects.get(slug=slug, author_id__in=subscriptions)
+
+
+def post_list(
+    *, filters=None, user: BaseUser, self_include: bool = True
+) -> QuerySet[Post]:
+    filters = filters or {}
+    subscriptions = list(
+        Subscription.objects.filter(subscriber=user).values_list("target", flat=True)
+    )
+    if self_include:
+        subscriptions.append(user.id)
+    if subscriptions:
+        qs = Post.objects.filter(author_id__in=subscription)
+        return PostFilter(filters, qs).qs
+    return Post.objects.none()
